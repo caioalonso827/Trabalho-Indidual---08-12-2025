05e25f0ea93eb19f8199ffeef252aab018001e0c
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-28 09:31:02 -0300

    Add files via upload

---
+++ src/main/java/com/senai/infoa/Api/para/ApiCall/Controller/EnderecoController.java
@@ -0,0 +1,24 @@
+package com.senai.infoa.Api.para.ApiCall.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.infoa.Api.para.ApiCall.Models.Endereco;
+import com.senai.infoa.Api.para.ApiCall.Services.EnderecoService;
+
+@RestController
+@RequestMapping("/Endereco")
+public class EnderecoController {
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @PostMapping("/Cadastrar")
+    public ResponseEntity<String> cadastrarEndereco (@RequestBody Endereco endereco) {
+        enderecoService.cadastrarEndereco(endereco);
+        return ResponseEntity.ok("Cadastrado com sucesso");
+    }
+}
