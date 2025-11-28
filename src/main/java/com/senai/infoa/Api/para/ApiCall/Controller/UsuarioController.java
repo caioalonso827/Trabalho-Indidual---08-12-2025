12802f61da4676985084234a859e23bd329925b0
Caio Borsato Alonso <caioca827@gmail.com>
2025-11-27 22:03:54 -0300

    Update UsuarioController.java

--- src/main/java/com/senai/infoa/Api/para/ApiCall/Controller/UsuarioController.java
+++ src/main/java/com/senai/infoa/Api/para/ApiCall/Controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.senai.infoa.Api.para.ApiCall.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @PostMapping("/Cadastrar")
     public ResponseEntity<String> cadastrarUsuario (@RequestBody Usuario usuario) {
         usuarioService.cadastrarUsuario(usuario);
         return ResponseEntity.ok("Cadastrado com Sucesso");
